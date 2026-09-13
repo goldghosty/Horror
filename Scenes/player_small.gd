082a706a0622e76f8ef32b571f44a50961b467d9
goldghosty <goldghosty@proton.me>
2026-09-13 13:11:20 -0700

    fixed idle images

--- Scenes/player_small.gd
+++ Scenes/player_small.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-
+var last_direction = "up"
+var is_moving = false
 func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -17,14 +18,31 @@ func _physics_process(delta: float) -> void:
 	if direction.x > 0:
 		animated_sprite_2d.flip_h = false
 		animated_sprite_2d.play("walking")
+		last_direction = "right"
+		is_moving = true
 	elif direction.y < 0:
 		animated_sprite_2d.play("walk_up")
+		last_direction = "up"
+		is_moving = true
 	elif direction.y > 0:
 		animated_sprite_2d.play("walk_down")
+		last_direction = "down"
+		is_moving = true
 	elif direction.x < 0:
 		animated_sprite_2d.flip_h = true
 		animated_sprite_2d.play("walking")
+		last_direction = "left"
+		is_moving = true
 	else:
 		animated_sprite_2d.play("idle")	
+		is_moving = false
+	
+	if last_direction == "up" and is_moving == false:
+		animated_sprite_2d.play("idle_up")
+	elif last_direction == "down" and is_moving == false:
+		animated_sprite_2d.play("idle_down")
+		
+		
+			
 
 	move_and_slide()
--- Scripts/bobcat.gd
+++ Scripts/bobcat.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
